d482a8e7f9b0a32ad57d46c8ba587da9968cb5c4
jordan-watson <jordanwatson000@gmail.com>
2024-01-18 18:04:21 +0000

    controls added

--- components/BaseCanvas.jsx
+++ components/BaseCanvas.jsx
@@ -1,17 +1,17 @@
-import { NighthawksModel } from './NighthawksModel'
-
+import { NighthawksModel } from "./NighthawksModel";
 
 function BaseCanvas() {
-    return (
-      <div id="canvas-container">
-        <Canvas>
+  return (
+    <div id="canvas-container">
+      <Canvas>
         <ambientLight intensity={0.1} />
         <PerspectiveCamera fov={75} position={[0, 0, 0]} makeDefault />
-        <Environment files={'./public/bar-enviro-for-export.glb'} background />
-            <NighthawksModel/>
-        </Canvas>
-      </div>
-    )
-  }
+        <Environment files={"./public/bar-enviro-for-export.glb"} background />
+        <NighthawksModel />
+        <PointerLockControls selector="#move-around" />
+      </Canvas>
+    </div>
+  );
+}
 
-  export default BaseCanvas
+export default BaseCanvas;
