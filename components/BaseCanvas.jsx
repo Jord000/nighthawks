494473555cfc5f92163c0e2308ec0a1baa78eab1
jordan-watson <jordanwatson000@gmail.com>
2024-01-22 18:59:31 +0000

    imrpove starting camera position

--- components/BaseCanvas.jsx
+++ components/BaseCanvas.jsx
@@ -14,9 +14,15 @@ import * as THREE from "three";
 
 function BaseCanvas() {
   const moveRef = useRef(null);
-  const [isCameraControl, setIsCameraControl] = useState(true)
+  const [isCameraControl, setIsCameraControl] = useState(false)
 
-useEffect(()=>{},[])
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'KeyO') {
+        setIsCameraControl(!isCameraControl)
+      }
+    })
+  })
 
   return (
     <div id="canvas-container" className="flex h-[100%] w-[100%]">
@@ -24,21 +30,23 @@ useEffect(()=>{},[])
         <ambientLight intensity={2} />
         <directionalLight color="white" position={[0, 0, 10]} />
         <PerspectiveCamera
-          fov={75}
-          position={[1.0, 1.8, 2.2]}
+          fov={76}
+          zoom={1.1}
+          position={[1.0, 1.6, 3]}
           rotation={[
-            THREE.MathUtils.degToRad(-10),
-            THREE.MathUtils.degToRad(45),
-            THREE.MathUtils.degToRad(10),
+            THREE.MathUtils.degToRad(-20),
+            THREE.MathUtils.degToRad(40),
+            THREE.MathUtils.degToRad(15),
           ]}
           makeDefault
         />
         <Environment files={"/SPACE-1.hdr"} background />
         <NighthawksModel />
         {/* <PointerLockControls selector="#move-around" /> */}
-        <CameraControls
+        {/* <OrbitControls
+         target={[10,10,10]}
          enabled={isCameraControl}
-       />
+       /> */}
       </Canvas>
       <Loader />
     </div>
