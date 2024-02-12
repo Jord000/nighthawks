7193a4166b42954e1737d60fba89562f9a2f5dc8
Jordan <jordanwatson000@gmail.com>
2024-02-12 21:37:32 +0000

    reduce paper glow

--- components/NewsPaper.jsx
+++ components/NewsPaper.jsx
@@ -1,40 +1,40 @@
-import { isMobileContext } from "@/contexts/isMobileContext";
-import { openInNewTab } from "@/utils/utils";
-import { useContext, useEffect, useState } from "react";
-import { LoopOnce } from "three";
+import { isMobileContext } from '@/contexts/isMobileContext'
+import { openInNewTab } from '@/utils/utils'
+import { useContext, useEffect, useState } from 'react'
+import { LoopOnce } from 'three'
 
 function NewsPaper({ nodes, materials, actions }) {
-  const [hovered, hover] = useState(false);
-  const { isMobile } = useContext(isMobileContext);
-  const [clicks, setClicks] = useState(0);
+  const [hovered, hover] = useState(false)
+  const { isMobile } = useContext(isMobileContext)
+  const [clicks, setClicks] = useState(0)
 
   const handlePointerDown = (e) => {
     if (isMobile) {
-      hover(true);
-      actions.BezierCurveAction.play();
-      setClicks(clicks + 1);
+      hover(true)
+      actions.BezierCurveAction.play()
+      setClicks(clicks + 1)
     }
-  };
+  }
 
   const handlePointerUp = (e) => {
     if (!isMobile || (isMobile && clicks > 1)) {
-      openInNewTab("https://thenewsjw.netlify.app/");
+      openInNewTab('https://thenewsjw.netlify.app/')
     } else if (isMobile) {
-      hover(true);
+      hover(true)
     }
-  };
+  }
 
   useEffect(() => {
-    actions.BezierCurveAction.clampWhenFinished = true;
-    actions.BezierCurveAction.setLoop(LoopOnce, 1);
-  }, []);
+    actions.BezierCurveAction.clampWhenFinished = true
+    actions.BezierCurveAction.setLoop(LoopOnce, 1)
+  }, [])
 
   return (
     <mesh
       name="newspaper"
       onPointerOver={() => {
-        hover(true);
-        actions.BezierCurveAction.play();
+        hover(true)
+        actions.BezierCurveAction.play()
       }}
       onPointerOut={() => hover(false)}
       onPointerDown={handlePointerDown}
@@ -48,13 +48,13 @@ function NewsPaper({ nodes, materials, actions }) {
     >
       {hovered && (
         <meshStandardMaterial
-          color={"white"}
-          emissive={"#7FCDF4"}
-          emissiveIntensity={hovered ? 5 : 0}
+          color={'white'}
+          emissive={'#7FCDF4'}
+          emissiveIntensity={hovered ? 2 : 0}
           toneMapped={false}
         />
       )}
     </mesh>
-  );
+  )
 }
-export default NewsPaper;
+export default NewsPaper
